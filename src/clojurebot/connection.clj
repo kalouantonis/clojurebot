1894709f4f5614c90b77a1d4d3993fbcd34b8c24
Antonis Kalou <kalouantonis@gmail.com>
2016-09-14 23:19:38 +0300

    Did some cleanup and removed some security vulnerabilities

---
+++ src/clojurebot/connection.clj
@@ -0,0 +1,75 @@
+(ns clojurebot.connection
+  (:require [clojure.string :as string])
+  (:import [java.net Socket]
+           [java.io PrintWriter InputStreamReader BufferedReader]))
+
+(defrecord Connection [in             ;; IRC input stream
+                       out            ;; IRC output stream
+                       active?        ;; True if connection is open
+                       channels       ;; Channels currently connected to
+                       msg-handler])  ;; Private message handler
+
+(defn- make-connection [in out msg-handler]
+  (map->Connection
+   {:in in
+    :out out
+    :active? false
+    :channels #{}
+    :msg-handler msg-handler}))
+
+(defn- write
+  "Write a string to the IRC output stream."
+  [conn msg]
+  (doto (:out @conn)
+    (.println (str msg "\r"))
+    (.flush)))
+
+(defn message [conn target & s]
+  (->> (string/join " " s)
+       (str "PRIVMSG " target " :")
+       (write conn)))
+
+(defn- kill
+  [conn]
+  (dosync (alter conn merge {:active? false})))
+
+;; TODO: Make reloadable
+(defn- conn-handler [conn]
+  (while (:active? @conn)
+    (let [msg (.readLine (:in @conn))]
+      (println msg)
+      (cond
+        (re-find #"^ERROR :Closing Link:" msg)
+        (kill conn)
+        (re-find #"PRIVMSG (.*) :(.*)" msg)
+        ;; TODO: Add configurable prefix
+        (let [[_ channel msg] (re-find #"PRIVMSG (.*) :@(.*)" msg)]
+          (message conn channel ((:msg-handler @conn) channel msg)))
+        (re-find #"^PING" msg)
+        (write conn (str "PONG " (re-find #":.*" msg)))))))
+
+(defn connect [server msg-handler]
+  ;; TODO: Close socket on finish
+  (let [socket (Socket. (:name server) (:port server))
+        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+        out (PrintWriter. (.getOutputStream socket))
+        conn (ref (make-connection in out msg-handler))]
+    (dosync (alter conn merge {:active? true})) ;; Start
+    (.start (Thread. #(conn-handler conn)))
+    conn))
+
+(defn login [conn user]
+  (write conn (str "NICK " (:nick user)))
+  (write conn (str "USER " (:nick user) " 0 * :" (:name user))))
+
+(defn quit [conn]
+  ;; This will send a :Closing link: message
+  (write conn "QUIT"))
+
+(defn join [conn channel]
+  (write conn (str "JOIN " channel))
+  (dosync (alter conn update :channels conj channel)))
+
+(defn part [conn channel]
+  (write conn (str "PART " channel))
+  (dosync (alter conn update :channels disj channel)))
--- src/clojurebot/core.clj
+++ src/clojurebot/core.clj
@@ -1,56 +1,24 @@
 (ns clojurebot.core
   (:gen-class)
-  (:require [clojure.string :as string]
-            [clojure.repl]
-            [clojail.core :as jail]
-            [clojail.testers :as testers])
-  (:import [java.net Socket]
-           [java.io PrintWriter InputStreamReader BufferedReader]))
+  (:require [clojail.core :as jail]
+            [clojail.testers :as testers]
+            [clojurebot.connection :as irc]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Clojure eval environment
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def sandbox (jail/sandbox testers/secure-tester :timeout 5000))
 
-(defn eval-message [msg]
+(defn eval-message
+  "Evaluate a message received from IRC."
+  [msg]
   (try
-    (sandbox (read-string msg))
+    (binding [*read-eval* false]
+      (sandbox (read-string msg)))
     (catch Exception e
       (.getMessage e))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; IRC Stuff
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def freenode {:name "irc.freenode.net" :port 6667})
-(def user {:name "The lispiest lisp bot" :nick "lispbot"})
-
-(declare conn-handler)
-
-(defn connect [server]
-  (let [socket (Socket. (:name server) (:port server))
-        in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
-        out (PrintWriter. (.getOutputStream socket))
-        conn (ref {:in in :out out :active? true :channels #{}})]
-    (.start (Thread. #(conn-handler conn)))
-    conn))
-
-(defn write [conn msg]
-  (doto (:out @conn)
-    (.println (str msg "\r"))
-    (.flush)))
-
-(defn kill
-  "Kill the given connection. This does not gracefully quit."
-  [conn]
-  (dosync (alter conn merge {:active? false})))
-
-(defn message [conn target & s]
-  (->> (string/join " " s)
-       (str "PRIVMSG " target " :")
-       (write conn)))
-
-(defn handle-message [msg]
+(defn handle-message [channel msg]
   (cond
+    (nil? msg)
+    msg
     (re-find #"^time$" msg)
     (.toString (java.util.Date.))
     (re-find #"^echo (.*)" msg)
@@ -69,40 +37,16 @@
            (:doc)))
     :else "ERROR: Unrecognised command"))
 
-(defn conn-handler [conn]
-  (while (:active? @conn)
-    (let [msg (.readLine (:in @conn))]
-      (println msg)
-      (cond
-        (re-find #"^ERROR :Closing Link:" msg)
-        (kill conn)
-        (re-find #"PRIVMSG (.*) :lispbot: (.*)" msg)
-        (let [[_ channel msg] (re-find #"PRIVMSG (.*) :lispbot: (.*)" msg)]
-          (message conn channel (handle-message msg)))
-        (re-find #"^PING" msg)
-        (write conn (str "PONG " (re-find #":.*" msg)))))))
-
-(defn login [conn user]
-  (write conn (str "NICK " (:nick user)))
-  (write conn (str "USER " (:nick user) " 0 * :" (:name user))))
-
-(defn quit [conn]
-  ;; This will send a :Closing link: message
-  (write conn "QUIT"))
-
-(defn join [conn channel]
-  (write conn (str "JOIN " channel))
-  (dosync (alter conn update :channels conj channel)))
-
-(defn part [conn channel]
-  (write conn (str "PART " channel))
-  (dosync (alter conn update :channels disj channel)))
+;; TODO: Load from config
+(def freenode {:name "irc.freenode.net" :port 6667})
+(def user {:name "The lispiest lisp bot" :nick "lispbot"})
 
 (defn -main
   "App entry point"
   [& args]
-  (let [irc (connect freenode)]
-    (login irc user)
+  (let [irc (irc/connect freenode handle-message)]
+    (irc/login irc user)
     (Thread/sleep 3000) ;; Just wait for a connection
-    (join irc "##system32")
-    (message irc "##system32" "Hello there!")))
+    (irc/join irc "##system32")
+    (irc/message irc "##system32" "Hello there!")
+    irc))
